5ca7596a4b9c10bb35a5b7af7ca3d0c4e8392998
Robert Wiggins <wigginsro11@gmail.com>
2019-07-01 14:18:13 -0400

    init commit, added basic input taking, skeleton, non-functional request tweets from backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import twitter from './apis/twitter'
 
-function App() {
+class App extends React.Component {
+
+  handleSearch(searchQuery) {
+    console.log(searchQuery) 
+  }
+
+  handleSubmitQuery(query) {
+    console.log(query);
+    twitter.retrieveTweets(query);
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className="search" id="searchOne" onSubmit={(e) => { 
+        e.preventDefault();
+        this.handleSubmitQuery(e.target.firstSearch.value);
+       }}>
+        <label>Search</label>
+        <input id="firstSearch" type="text" name="firstSearch" onChange={(e) => this.handleSearch(e.target.value)}></input>
+        <button>Submit</button>
+      </form>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/apis/twitter/twitter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import app from './App';
+import { get } from 'http';
+import config from '../../../config'
+
+class twitter {
+  retrieveTweets(query) {
+    const options = {
+      method: 'GET',
+      'content-type': 'application/json',
+      body: {
+        query,
+      },
+    };
+
+    fetch(config.API_ENDPOINT + '/tweets/queries', options)
+      .then(response => {
+        if (!response.ok) {
+          return {
+            status: response.status,
+            message: 'something seems to have gone wrong',
+          };
+        }
+        return response.json();
+      })
+      .catch(error => error); // perhaps customize more
+  }
+}
