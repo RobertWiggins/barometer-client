765b086b7588ed49491490c8ca80320c968c08f2
Robert Wiggins <wigginsro11@gmail.com>
2019-07-16 11:09:25 -0400

    added landing screenshot

--- src/Components/LandingDescription/LandingDescription.js
+++ src/Components/LandingDescription/LandingDescription.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Landing.css'
+import './Landing.css';
 
 export default class LandingDescription extends React.Component {
   render() {
     return (
       <div id="landingPane">
-        <section className="landingRow" >
+        <section className="landingRow">
           <header role="banner">
             <h3>
               Determine which emotions people on Twitter are feeling regarding
@@ -13,27 +13,30 @@ export default class LandingDescription extends React.Component {
             </h3>
           </header>
           <p>
-            <em>
-              placeholder for screenshot of natural language emotion summary per
-              query.
-            </em>
-          </p>
-          <p>
             The Barometer web application grants users the ability to
             quantitatively measure the general population’s emotional view of
-            events and subjects in real-time.
+            events and subjects on Twitter.
           </p>
-            <p> Through combining Twitter's tweet
-            search result API and IBM’s natural language processing model, the
-            results are aggregated into 6 defined emotional sentiment categories
-            (%): sadness, joy, fear, disgust, anger, and overall positive vs.
-            negative outlook.
-            </p> 
-            <p id="thesis">The application is effectively a continuous,
-            user-search driven online social presence survey.
-            </p> 
+          <p>
+            {' '}
+            Through combining Twitter's tweet search result API and IBM’s
+            natural language processing model, the results are aggregated into 6
+            emotional sentiment categories (%): sadness, joy, fear, disgust,
+            anger, and overall positive vs. negative outlook.
+          </p>
+          <p>
+            <img
+              alt="Search query 'leonard clippers trade' with tweet results and emotion target keywords chart, indicating dominating sadness and joy emotional composition"
+              src="./static/search_home.png"
+              id="search-home-img"
+            />
+          </p>
+          <p id="thesis">
+            The application is a continuous, user-search driven online social
+            presence survey.
+          </p>
         </section>
-        <section id="callAction" >
+        <section id="callAction">
           <header role="banner">
             <h3>
               Record and quantify the online users' emotional viewpoint on any
@@ -41,17 +44,16 @@ export default class LandingDescription extends React.Component {
             </h3>
           </header>
           <p>
-            <em>
-              placeholder for screenshot of emotion chart regarding subject
-              matter.
-            </em>
-          </p>
-          <p>
-            Interactive charts and comparative displays enable easily digestible
+            Interactive charts and comparative displays enable digestible
             proprietary insights.
           </p>
         </section>
-        <button id="hideLanding" onClick={() => this.props.onLandingButtonClick()}>Start searching now.</button>
+        <button
+          id="hideLanding"
+          onClick={() => this.props.onLandingButtonClick()}
+        >
+          Start searching now.
+        </button>
       </div>
     );
   }
